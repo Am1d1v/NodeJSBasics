65a00518e66f5b3fcc4aaeefbad122fd2695ef0e
Am1d1v <dimadiman1adima@gmail.com>
2024-08-21 14:08:33 +0300

    Sending Response

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const http = require('http');
 const server =  http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Response Text</title></head>');
+    res.write('<body><h1>Response</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
