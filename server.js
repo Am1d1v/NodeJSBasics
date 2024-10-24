1559b6052e3b523ec9e9cb7f80baa3af3449a88e
Am1d1v <dimadiman1adima@gmail.com>
2024-10-25 00:25:24 +0300

    Nodemon

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const http = require('http');
-const requestHandler = require('./routes');
+const routes = require('./routes');
 
-const server =  http.createServer((req, res) => {
-
-    requestHandler(req, res);
-
-});
+const server =  http.createServer(routes);
 
 server.listen(3000);
