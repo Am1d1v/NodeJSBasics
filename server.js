fc1b9e4ff3aa9c18b841539ca2cf67d4927617eb
Am1d1v <dimadiman1adima@gmail.com>
2024-10-25 13:57:33 +0300

    NodeJS Middleware

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require('express');
 //const routes = require('./routes');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the 1st middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the 2nd middleware');
+});
+
 const server =  http.createServer(app);
 
 server.listen(3000);
