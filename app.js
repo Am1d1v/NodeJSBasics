30a762330048c6350e8d85d36a1084b846265115
Am1d1v <dimadiman1adima@gmail.com>
2024-10-26 19:35:23 +0300

    Handle 404 Error Page

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Add product page
 app.use(adminRoutes);
 
+// Home page
 app.use(shopRoutes);
+
+// Handle 404 Page
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
